8fec9190319eecf440f1983d0f51951e4f45fdd0
NaHCO3 <1318003424@qq.com>
2023-04-25 16:47:06 +0800

    前端-9-删除成功

--- canteen-frontend/src/main.js
+++ canteen-frontend/src/main.js
@@ -6,8 +6,12 @@ import 'element-ui/lib/theme-chalk/index.css';
 
 import './assets/gloable.css'
 
+import request from "@/utils/request";
+
 Vue.config.productionTip = false;
 Vue.use(ElementUI, {size: "small"});
+
+Vue.prototype.request = request
 
 new Vue({
   router,
--- canteen-frontend/src/views/HomeView.vue
+++ canteen-frontend/src/views/HomeView.vue
@@ -78,17 +78,20 @@
 
                 <!--        搜索部分        -->
                 <div style="margin: 10px 0">
-                    <el-input style="width: 200px" placeholder="请输入名称" suffix-icon="el-icon-search" v-model="username"></el-input>
+                    <el-input style="width: 200px" placeholder="请输入名称" suffix-icon="el-icon-search"
+                              v-model="username"></el-input>
                     <el-input style="width: 200px" placeholder="请输入邮箱" suffix-icon="el-icon-message"
-                              class="ml-5"></el-input>
+                              class="ml-5" v-model="email"></el-input>
                     <el-input style="width: 200px" placeholder="请输入地址" suffix-icon="el-icon-position"
-                              class="ml-5"></el-input>
+                              class="ml-5" v-model="address"></el-input>
                     <el-button class="ml-5" type="primary" @click="load">搜索</el-button>
+                    <el-button class="ml-5" type="warning" @click="reset">重置</el-button>
                 </div>
 
                 <!--      表格外部操作部分          -->
                 <div style="margin: 10px 0">
-                    <el-button type="primary">新增 <i class="el-icon-circle-plus-outline"></i></el-button>
+                    <el-button type="primary" @click="handleAdd">新增 <i class="el-icon-circle-plus-outline"></i>
+                    </el-button>
                     <el-button type="danger">批量删除 <i class="el-icon-remove-outline"></i></el-button>
                     <el-button type="primary">导入 <i class="el-icon-bottom"></i></el-button>
                     <el-button type="primary">导出 <i class="el-icon-top"></i></el-button>
@@ -104,8 +107,18 @@
                     <el-table-column prop="address" label="地址"></el-table-column>
                     <el-table-column label="操作" width="200" align="center">
                         <template slot-scope="scope">
-                            <el-button type="success">编辑 <i class="el-icon-edit"></i></el-button>
-                            <el-button type="danger">删除 <i class="el-icon-remove-outline"></i></el-button>
+                            <el-button type="success" @click="handleEdit(scope.row)">编辑 <i class="el-icon-edit"></i>
+                            </el-button>
+                            <el-popconfirm
+                                    class="ml-5"
+                                    confirm-button-text='确定'
+                                    cancel-button-text='我再想想'
+                                    icon="el-icon-info"
+                                    icon-color="red"
+                                    title="您确定删除吗？"
+                                    @confirm="del(scope.row.id)">
+                                <el-button type="danger" slot="reference">删除 <i class="el-icon-remove-outline"></i></el-button>
+                            </el-popconfirm>
                         </template>
                     </el-table-column>
                 </el-table>
@@ -113,8 +126,8 @@
                 <!--       翻页与页码部分         -->
                 <div style="padding: 10px 0">
                     <el-pagination
-                        @size-change="handleSizeChange"
-                        @current-change="handleCurrentChange"
+                            @size-change="handleSizeChange"
+                            @current-change="handleCurrentChange"
                             :current-page="pageNum"
                             :page-sizes="[2, 5, 10, 20]"
                             :page-size="pageSize"
@@ -122,6 +135,30 @@
                             :total="total">
                     </el-pagination>
                 </div>
+
+                <el-dialog title="用户信息" :visible.sync="dialogFormVisible" width="30%">
+                    <el-form label-width="80px" size="small">
+                        <el-form-item label="用户名">
+                            <el-input v-model="form.username" autocomplete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item label="昵称">
+                            <el-input v-model="form.nickname" autocomplete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item label="邮箱">
+                            <el-input v-model="form.email" autocomplete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item label="电话">
+                            <el-input v-model="form.phone" autocomplete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item label="地址">
+                            <el-input v-model="form.address" autocomplete="off"></el-input>
+                        </el-form-item>
+                    </el-form>
+                    <div slot="footer" class="dialog-footer">
+                        <el-button @click=" dialogFormVisible = false ">取 消</el-button>
+                        <el-button type="primary" @click="save">确 定</el-button>
+                    </div>
+                </el-dialog>
 
             </el-main>
 
@@ -143,8 +180,12 @@ export default {
             tableData: [],
             total: 0,
             pageNum: 1,
-            pageSize: 2,
+            pageSize: 10,
             username: "",
+            email: "",
+            address: "",
+            form: {},
+            dialogFormVisible: false,
             msg: "IronmanJay",
             collapseBtnClass: 'el-icon-s-fold',
             isCollapse: false, //默认侧边栏打开
@@ -167,21 +208,65 @@ export default {
             }
         },
         load() {
-            fetch("http://localhost:9090/user/page?pageNum=" + this.pageNum + "&pageSize=" + this.pageSize+"&username="+this.username).then(res => res.json()).then(res => {
+            this.request.get("/user/page", {
+                params: {
+                    pageNum: this.pageNum,
+                    pageSize: this.pageSize,
+                    username: this.username,
+                    email: this.email,
+                    address: this.address
+                }
+            }).then(res => {
                 console.log(res)
-                this.tableData = res.data
+                this.tableData = res.records
                 this.total = res.total
             })
         },
-        handleSizeChange(pageSize) {
-            console.log(pageSize)
-            this.pageSize=pageSize
+        reset() {
+            this.username = ""
+            this.email = ""
+            this.address = ""
             this.load()
         },
-        handleCurrentChange(pageNum){
-            console.log(pageNum)
-            this.pageNum=pageNum
+        handleSizeChange(pageSize) {
+            console.log(pageSize)
+            this.pageSize = pageSize
             this.load()
+        },
+        handleCurrentChange(pageNum) {
+            console.log(pageNum)
+            this.pageNum = pageNum
+            this.load()
+        },
+        handleAdd() {
+            this.dialogFormVisible = true
+            this.form = {}
+        },
+        handleEdit(row) {
+            this.form = row
+            this.dialogFormVisible = true
+        },
+        del(id){
+            this.request.delete("/user/"+id).then(res => {
+                if(res){
+                    this.$message.success("删除成功！")
+                    this.load()
+                }else {
+                    this.$message.error("删除失败！")
+                }
+            })
+        },
+        save() {
+            this.request.post("/user", this.form).then(res => {
+                if (res) {
+                    this.$message.success("保存成功！")
+                    this.dialogFormVisible = false
+                    this.load()
+                } else {
+                    this.$message.error("保存失败！")
+                    this.dialogFormVisible = false
+                }
+            })
         }
     }
 }
