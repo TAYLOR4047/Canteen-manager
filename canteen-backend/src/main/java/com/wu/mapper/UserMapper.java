05ad200abe252543534ea646530488a1f5cd30ee
NaHCO3 <1318003424@qq.com>
2023-04-25 11:45:06 +0800

    后端模糊查询的实现

--- canteen-backend/src/main/java/com/wu/mapper/UserMapper.java
+++ canteen-backend/src/main/java/com/wu/mapper/UserMapper.java
@@ -9,11 +9,12 @@ import java.util.List;
 @Mapper
 public interface UserMapper{
     //查询所有数据
-    @Select("select * from user limit #{pageNum},#{pageSize}")
-    List<User> selectPage(Integer pageNum, Integer pageSize);
+    @Select("select * from user where username like concat('%',#{username},'%') limit #{pageNum},#{pageSize}")
+    List<User> selectPage(Integer pageNum, Integer pageSize,String username);
 
-    @Select("select count(*) from user")
-    Integer selectTotal();
+
+    @Select("select count(*) from user where username like concat('%',#{username},'%')")
+    Integer selectTotal(String username);
 
     @Select("select * from user")
     List<User> findAll();
