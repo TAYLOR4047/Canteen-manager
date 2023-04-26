ddd0e0819ad98a38de977a0b91d35809dc63ba70
NaHCO3 <1318003424@qq.com>
2023-04-26 10:30:30 +0800

    前端11-VueX

--- canteen-frontend/src/main.js
+++ canteen-frontend/src/main.js
@@ -6,7 +6,9 @@ import 'element-ui/lib/theme-chalk/index.css';
 
 import './assets/gloable.css'
 
+
 import request from "@/utils/request";
+import store from "@/store/store";
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI, {size: "small"});
@@ -15,5 +17,6 @@ Vue.prototype.request = request
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app');
